9b3d5606719607e80597e3454b105662aaa500e9
Arthur Avila <arthurpro295@gmail.com>
2025-12-02 11:26:41 -0300

    Inserindo novos componentes

---
+++ src/components/ConteudoPrincipal.vue
@@ -0,0 +1,38 @@
+<script lang="ts">
+import SelecionarIngrediente from './SelecionarIngrediente.vue';
+import SuaLista from './SuaLista.vue';
+
+
+export default {
+    data() {
+        return {
+            ingredientes: ['Alho', 'Manteiga', 'Orégano']
+        }
+    },
+    components: {
+        SelecionarIngrediente,
+        SuaLista
+    }
+};
+</script>
+
+<template>
+    <main class="conteudo-principal">
+        <SuaLista :ingredientes="ingredientes" />
+        <SelecionarIngrediente />
+    </main>
+</template>
+
+<style scoped>
+.conteudo-principal {
+    padding: 6.5rem 7.5rem;
+    border-radius: 3.75rem 3.75rem 0rem 0rem;
+    background: var(--creme, #FFFAF3);
+    color: var(--cinza, #444);
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 5rem;
+}
+</style>
